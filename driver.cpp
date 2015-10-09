05edef38c4d5183530f19a15c0dc0a539958365e
GiselleBeltran <gisellembeltran@student.hartnell.edu>
2015-10-09 10:51:14 -0700

    Create driver.cpp

---
+++ driver.cpp
@@ -0,0 +1,21 @@
+# adventure
+
+// Labwork - Team Designing
+
+//includes
+
+//prototypes
+char menu();
+double climbing();
+double scuba();
+double skyDive();
+double spelunk();
+
+
+int main ()
+{
+
+    return 0;
+}
+
+//function definitions
