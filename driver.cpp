a486765d3e6c4f1d0291aadbac18abc3e85c4442
Alexis <jwelch@hartnell.edu>
2015-10-12 10:13:48 -0700

    functions climbing and scuba added

--- driver.cpp
+++ driver.cpp
@@ -1,21 +1,98 @@
-# adventure
+//s# adventure
 
 // Labwork - Team Designing
 
 //includes
-
+#include<iostream>
+#include<cassert>
+using namespace std;
 //prototypes
-char menu();
-double climbing();
-double scuba();
-double skyDive();
-double spelunk();
+//char menu();
+double climbing(int people);
+double scuba(int people);
+//double skyDive();
+double discount(int people);
+int numberOfPeople();
+//double spelunk();
 
 
 int main ()
 {
+	int people = numberOfPeople();
+	int discountPrice;
+	discountPrice = discount(people);
+	
+	cout << scuba( people) << endl;
 
     return 0;
 }
 
 //function definitions
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int numberOfPeople()
+{
+	int people;
+	cout << "List number of people going:\n";
+	cin >> people;
+	return people;
+}
+double discount(int people)
+{
+	double discountPrice;
+	if(people >= 5)
+		discountPrice = (0.10);
+	else 
+		discountPrice = (0.0);
+	return discountPrice;
+}
+double climbing(int people){
+    double priceTotal;
+	string climbingInstruction, equipRental;
+	
+    cout << "Will you need any climbing instruction?\n";
+    cin >> climbingInstruction;
+    cout << "Will you need any equipment rentals?\n";
+    cin >> equipRental;
+    priceTotal = (people * 350.00);
+    if ((climbingInstruction == "Yes") || (climbingInstruction == "yes")){
+        priceTotal += (people*100);
+    }
+    if ((equipRental == "Yes") || (equipRental == "yes")){
+        priceTotal += (people*40)*(3);
+    }
+    if ( people >= 5){
+    	priceTotal -= (priceTotal*(discount(people)));
+    }
+    return priceTotal;
+}
+double scuba(int people){
+	double priceTotal;
+	string scubaInstruction;
+	
+	cout << "Will you need any scuba instruction\n";
+	cin >> scubaInstruction;
+	
+	priceTotal = (people * 1000.00);
+	if ((scubaInstruction == "Yes") || (scubaInstruction == "yes")){
+		priceTotal += (people * 100);
+	}
+	return priceTotal;
+}
+
+
+
+
